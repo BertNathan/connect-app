a0aa02b7405ed98d6459f4b2794d2c331359d114
BertNathan <bertenlyrippo16@gmail.com>
2025-06-04 10:24:03 +0800

    Wawasodesoo

--- src/app/admin/roles/page.js
+++ src/app/admin/roles/page.js
@@ -7,31 +7,12 @@ import {
 import Link from "next/link";
 import RolesCard from "@/components/ui/tablerole";
 import { dataroles } from "../../../../mock/roledata";
+import AdminLayout from "../layout";
 
 export default function RolesPage() {
   return (
     <section id="container" className="flex h-screen justify-center">
-      <section
-        id="navigation"
-        className="bg-white w-[15%] border-r border-gray-300 p-4 "
-      >
-        <h1 className="text-[30px] mb-5 font-sans font-bold text-center">
-          Connect
-        </h1>
-        <div className="nav-buttons flex flex-col items-center gap-4 p-2 w-full mb-2 font-bold text-[18px]">
-          <Link href="../admin/users">
-            <button className="rounded-[10px] flex items-center gap-2 py-2 px-3 text-black cursor-pointer">
-              <IconUser /> Users
-            </button>
-          </Link>
-          <button className="rounded-[10px] flex items-center gap-2 py-2 px-3 bg-black text-white border border-gray-300 w-[90%] cursor-pointer">
-            <IconUsersPlus /> Hak Akses
-          </button>
-          <button className="flex items-center gap-2 py-2 px-3 w-[90%]">
-            <IconLogout2 /> Logout
-          </button>
-        </div>
-      </section>
+      
       <section id="content" className="bg-white w-[85%] p-5">
         <input
           type="text"
